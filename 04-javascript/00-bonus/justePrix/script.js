622e8e90c3078eeb345716110a802f18b03d5d8b
Hiromi-k57 <nim57oom@gmail.com>
2025-03-26 23:10:35 +0100

    04JS_00_justePrix edited

--- 04-javascript/00-bonus/justePrix/script.js
+++ 04-javascript/00-bonus/justePrix/script.js
@@ -21,60 +21,76 @@ Lorsque le jeu est terminé, les boutons et input doivent être désactivé.
 6. (bonus) Le jeu serait plus vivant si il est animé avec par exemple une carte qui se retourne pour afficher le résultat et le bouton recommencer.
 */
 
-const cardInner = document.getElementsByClassName("card-inner");
-const result = document.querySelector("p");
-const input = document.querySelector("input");
-const submitBtn = document.querySelector("#submit");
-const resetBtn = document.querySelector("#reset");
-let guessCount = 1;
+"use strict"
 
-console.log(cardInner,result,input,submitBtn,resetBtn);
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  const guesses = document.querySelector('.guesses');
+  const lastResult = document.querySelector('.lastResult');
+  const lastResult2 = document.querySelector('.lastResult2');
+  const lowOrHi = document.querySelector('.lowOrHi');
+  const guessSubmit = document.querySelector('.guessSubmit');
+  const guessField = document.querySelector('.guessField');
+  const card = document.querySelector('.card');
+  let guessCount = 1;
+  let resetButton;
 
-cardInner.addEventListener('click', function() {
-    turnover();
-  });
-  function turnover() {
-    if(card.className = card.className === ''){
-      card.className = 'card-inner';
-    }else{
-      card.className = '';
+ 
+  function checkGuess() {
+    const userGuess = Number(guessField.value);
+     if (userGuess === randomNumber) {
+      lastResult.textContent = 'Bien joué ! Vous avez trouvé en 7 tours';
+      lastResult.style.backgroundColor = 'green';
+    //   lastResult2.style.display = 'block', randomNumber;
+    //   appendChild(randomNumber);
+      lowOrHi.textContent = '';
+      setGameOver();
+     } 
+     
+     else if (guessCount === 7) {
+      lastResult.textContent = 'C\'est perdu !';
+      lastResult.style.backgroundColor = 'red';
+      lowOrHi.textContent = '';
+      setGameOver();
+     } else {
+    //   lastResult.textContent = '間違いです!';
+    //   lastResult.style.backgroundColor = 'red';
+      if(userGuess < randomNumber) {
+        lowOrHi.textContent = `C\'est plus grand que ${userGuess} !` ;
+      } else if(userGuess > randomNumber) {
+        lowOrHi.textContent = `C\'est plus petit que ${userGuess} !` ;
+      }
     }
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
   }
 
+   guessSubmit.addEventListener('click', checkGuess);
 
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-function checkAnswer(){
-    const userAnswer = Number(input.value);
-    if (guessCount === 1) {
-      result.textContent = 'Vouillez choisir un nombre entre 1 et 100, vous avez 7 essais ';
-    }
-
-    let message;
-    if (userAnswer === randomNumber) {
-        result.textContent = "Bien joué ! Vous avez trouvé en 7 tours"
-        input.disabled = true;    
-    } 
-    else{
-        message ="";
-        if(userAnswer >randomNumber){
-            message = "C'est plus petit que" + userAnswer + "!";
-        }
-        if(userAnswer < randomNumber){
-            message = "C'est plus grand que" + userAnswer + "!";
-        }
-    }
+    function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Recommencer';
+    card.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
     
-}
 
+  }
 
-
-
-
-
-
-
-
-
-
-
+  function resetGame() {
+   guessCount = 1;
+   const resetParas = document.querySelectorAll('.resultP p');
+    for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+    }
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+    lastResult.style.backgroundColor = 'white';
+    // lastResult.textContent = 'Vouillez choisir un nombre entre 1 et 100, vous avez 7 essais';
+    // lastResult.style.fontColor = 'black';
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+  }
