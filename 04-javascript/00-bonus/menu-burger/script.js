275a5b29dda8c1ccb7f31fe6153763163f0952e3
Hiromi-k57 <nim57oom@gmail.com>
2025-05-12 22:39:25 +0200

    js exo

---
+++ 04-javascript/00-bonus/menu-burger/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.querySelector('.hamburger').addEventListener('click', function(){
+  this.classList.toggle('active');
+  document.querySelector('.slide-menu').classList.toggle('active');
+})
